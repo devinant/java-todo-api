bf6ed05400f650b0eb2b6bf59075beeb3ba9b99f
Dino Opijac <dino.opijac@gmail.com>
2016-05-03 23:31:02 +0200

    Todos->Todo

---
+++ src/test/java/ac/opij/todo/resources/TodoResourceTest.java
@@ -0,0 +1,68 @@
+package ac.opij.todo.resources;
+
+import ac.opij.todo.core.Todo;
+import ac.opij.todo.db.TodoDAO;
+import io.dropwizard.testing.junit.ResourceTestRule;
+import org.glassfish.jersey.test.grizzly.GrizzlyWebTestContainerFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+import java.util.Optional;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TodoResourceTest {
+    private static final TodoDAO DAO = mock(TodoDAO.class);
+
+    @ClassRule
+    public static final ResourceTestRule RULE = ResourceTestRule.builder()
+            .addResource(new TodoResource(DAO))
+            .setTestContainerFactory(new GrizzlyWebTestContainerFactory())
+            .build();
+
+    private Todo todo;
+
+    @Before
+    public void setUp() throws Exception {
+        this.todo = new Todo();
+        this.todo.setId(1L);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        reset(DAO);
+    }
+
+    @Test
+    public void testTodoWasFound() throws Exception {
+        when(DAO.findById(1L)).thenReturn(Optional.of(this.todo));
+
+        Todo todo = RULE.getJerseyTest().target("/todo/1")
+                .request()
+                .get(Todo.class);
+
+        assertThat(todo.getId()).isEqualTo(this.todo.getId());
+        verify(DAO).findById(1L);
+    }
+
+    @Test
+    public void testTodoNotFound() throws Exception {
+        when(DAO.findById(2L)).thenReturn(Optional.empty());
+
+        final Response response = RULE.getJerseyTest()
+                .target("/todo/2")
+                .request()
+                .get();
+
+        assertThat(response.getStatusInfo().getStatusCode()).isEqualTo(Response.Status.NOT_FOUND.getStatusCode());
+        verify(DAO).findById(2L);
+    }
+}
--- src/test/java/ac/opij/todo/resources/TodosResourceTest.java
+++
@@ -1,7 +0,0 @@
-package ac.opij.todo.resources;
-
-/**
- * Created by dino on 03/05/16.
- */
-public class TodosResourceTest {
-}
